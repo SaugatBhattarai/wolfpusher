47eb48cd1e626266205e353111e87b7f2668c0a1
Saugat Bhattarai <saugatbhattarai@outlook.com>
2016-08-29 10:57:10 +0545

    Initial Commit

---
+++ service-worker.js
@@ -0,0 +1,49 @@
+
+self.addEventListener('install', function(event) {
+  self.skipWaiting();
+  console.log('Installed', event);
+});
+
+self.addEventListener('activate', function(event) {
+  console.log('Activated', event);
+});
+
+self.addEventListener('push', function(event) {
+  console.log('Push message received', event);
+  // TODO
+});	
+
+
+self.addEventListener('message', function(event){
+    console.log("SW Received Message: " + event.data);
+    event.ports[0].postMessage("SW Says 'Hello back!'");
+});
+
+
+function send_message_to_client(client, msg){
+    return new Promise(function(resolve, reject){
+        var msg_chan = new MessageChannel();
+
+        msg_chan.port1.onmessage = function(event){
+            if(event.data.error){
+                reject(event.data.error);
+            }else{
+                resolve(event.data);
+            }
+        };
+
+        client.postMessage("SW Says: '"+msg+"'", [msg_chan.port2]);
+    });
+}
+
+function send_message_to_all_clients(msg){
+    clients.matchAll().then(clients => {
+        clients.forEach(client => {
+            send_message_to_client(client, msg).then(m => console.log("SW Received Message: "+m));
+        })
+    })
+}
+
+// console.log(self.clients);
+// console.log(self.clients.get(id));
+// console.log(clientId);
