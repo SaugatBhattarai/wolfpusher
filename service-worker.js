3fd4ad96d207df40784c88c178984bd664d8a061
Saugat Bhattarai <saugat.bhattarai@outlook.com>
2016-08-30 16:32:34 +0545

    specific link push from service worker console

--- service-worker.js
+++ service-worker.js
@@ -22,24 +22,22 @@ self.addEventListener('message', function(event){
 
 function send_message_to_client(client, msg){
     return new Promise(function(resolve, reject){
-        var msg_chan = new MessageChannel();
+        var messageChannel = new MessageChannel();
 
-        msg_chan.port1.onmessage = function(event){
+        messageChannel.port1.onmessage = function(event){
             if(event.data.error){
                 reject(event.data.error);
             }else{
                 resolve(event.data);
             }
         };
-
-        client.postMessage("SW Says: '"+msg+"'", [msg_chan.port2]);
+        client.postMessage("SW Says: '"+msg+"'", [messageChannel.port2]);
     });
 }
 
 function send_message_to_specific_client(msg){
   clients.matchAll().then(function(clients) {
     console.log(clients);
-    // console.log(clients.id);
      var i=0;
      clients.forEach(client => {      
       if(i==0){
@@ -51,6 +49,18 @@ function send_message_to_specific_client(msg){
   });
 }
 
+function send_message_to_specific_link(msg){
+clients.matchAll().then(function(clients) {
+  var messageChannel = new MessageChannel();
+    for(var i = 0 ; i < clients.length ; i++) {
+          if(clients[i].url === 'http://localhost/wolfpusher/client2.html') {
+            send_message_to_client(clients[i], msg).then(m => console.log("SW Received Message: "+m)); 
+          }
+    }
+  });
+}
+
+
 
 function send_message_to_all_clients(msg){
     clients.matchAll().then(clients => {
@@ -59,7 +69,3 @@ function send_message_to_all_clients(msg){
         })
     })
 }
-
-// console.log(self.clients);
-// console.log(self.clients.get(id));
-// console.log(clientId);
