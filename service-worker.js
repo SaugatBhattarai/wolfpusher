535c4f6562ae31834be5d66227f78ce9c15423ff
Saugat Bhattarai <saugat.bhattarai@outlook.com>
2016-09-06 22:25:14 +0545

    Subscribe and unsubscribe unique browser id

--- service-worker.js
+++ service-worker.js
@@ -9,10 +9,16 @@ self.addEventListener('activate', function(event) {
 });
 
 self.addEventListener('push', function(event) {
-  console.log('Push message received', event);
-  // TODO
+  console.log('This is test push. Push notification');
 });	
 
+// self.addEventListener('fetch',function(event){
+//   // console.log(event.request);
+//   console.log(event.request.url);
+//   console.log(event.request.method);
+//   console.log(event.request.headers);
+//   // event.responseWith(new response("Hello World Saugat His is Page hijacks"));
+// });
 
 self.addEventListener('message', function(event){
     console.log("SW Received Message: " + event.data);
@@ -38,14 +44,14 @@ function send_message_to_client(client, msg){
 function send_message_to_specific_client(msg){
   clients.matchAll().then(function(clients) {
     // console.log(clients);
-     var i=0;
-     clients.forEach(client => {      
+    var i=0;
+    clients.forEach(client => {      
       if(i==0){
         console.log('sent '+i);
         send_message_to_client(client, msg).then(m => console.log("SW Received Message: "+m));
       }
-      i++;
-        })
+        i++;
+    })
   });
 }
 
@@ -53,9 +59,9 @@ function send_message_to_specific_link(msg){
 clients.matchAll().then(function(clients) {
   var messageChannel = new MessageChannel();
     for(var i = 0 ; i < clients.length ; i++) {
-          if(clients[i].url === 'http://localhost/wolfpusher/client2.html') {
-            send_message_to_client(clients[i], msg).then(m => console.log("SW Received Message: "+m)); 
-          }
+      if(clients[i].url === 'http://localhost/wolfpusher/client2.html') {
+        send_message_to_client(clients[i], msg).then(m => console.log("SW Received Message: "+m)); 
+      }
     }
   });
 }
@@ -64,19 +70,18 @@ function send_message_to_specific_given_link(client,msg){
 clients.matchAll().then(function(clients) {
   var messageChannel = new MessageChannel();
     for(var i = 0 ; i < clients.length ; i++) {
-          if(clients[i].url === "http://localhost/wolfpusher/"+client){
-            send_message_to_client(clients[i], msg).then(m => console.log("SW Received Message: "+m)); 
-          }
+      if(clients[i].url === "http://localhost/wolfpusher/"+client){
+        send_message_to_client(clients[i], msg).then(m => console.log("SW Received Message: "+m));
+      }
     }
   });
 }
 
-
-
 function send_message_to_all_clients(msg){
     clients.matchAll().then(clients => {
-        clients.forEach(client => {
-            send_message_to_client(client, msg).then(m => console.log("SW Received Message: "+m));
-        })
+      clients.forEach(client => {
+          send_message_to_client(client, msg).then(m => console.log("SW Received Message: "+m));
+      })
     })
 }
+
